39bb8e0e3e375be3d7a0f10c12437972816c8c71
Cesar-Hacker <44127894+Cesar-Hacker@users.noreply.github.com>
2018-10-24 20:33:04 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,36 @@
+
+
+clear
+
+setterm -foreground cyan
+figlet Cesar Hack
+setterm -foreground blue
+	echo '======Cesar=Hacker=The=Gray================='
+apt update
+apt upgrade
+chmod +x /data/data/com.termux/files/home/tmvenom/run
+chmod +x /data/data/com.termux/files/home/tmvenom/tmvenom_pro.py
+apt update && apt upgrade -y
+apt install python2
+apt install openssh
+mkdir -p $HOME/tmvenom/core
+mv /data/data/com.termux/files/home/tmvenom/run /data/data/com.termux/files/home/tmvenom/core
+apt install curl
+apt install perl
+apt install ruby
+gem install lolca///t
+pip install requests
+pip2 install requests
+pip install colorama
+pip2 install colorama
+figlet -f shadow  CesaR HackeR
+setterm -foreground cyan
+	echo "          Listoo!! ejecutamos tmvenom"
+setterm -foreground green	
+	echo "          python2 tmvenom_pro.py"
+	
+	echo "          para conectarnos con ssh asemos lo siguente"
+setterm -foreground cyan
+	echo "          ssh -R 81929:(IP):4444 serveo.net"
+	
+	echo "          Hackeando fuera de la red sin ngrok"
